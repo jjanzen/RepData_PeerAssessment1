83edfbe17f1b448d7dd376b09028bda2625ba553
jjanzen <joshua.janzen@bestbuy.com>
2015-02-14 13:08:57 -0600

    Initial Commit for peer assessment

--- PA1_template.md
+++ PA1_template.md
@@ -1,14 +1,10 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
+# Reproducible Research: Peer Assessment 1
 
 
 ## Loading and preprocessing the data
 
-```{r}
+
+```r
 setwd("~/JHDataScience/Reproducable_Research/RepData_PeerAssessment1")
 unzip(zipfile="activity.zip")
 activity_df <- read.csv("activity.csv")
@@ -17,27 +13,69 @@ library(ggplot2)
 library(sqldf)
 ```
 
+```
+## Loading required package: gsubfn
+## Loading required package: proto
+## Loading required package: RSQLite
+## Loading required package: DBI
+```
+
 ## What is mean total number of steps taken per day?
-```{r}
+
+```r
 stepsByDay <- tapply(activity_df$steps, activity_df$date, sum, na.rm=T)
 stepsByDayMean <- mean(stepsByDay)
 stepsByDayMedian <- median(stepsByDay)
 stepsByDayMean
-stepsByDayMedian
+```
 
+```
+## [1] 9354.23
+```
+
+```r
+stepsByDayMedian
+```
+
+```
+## [1] 10395
+```
+
+```r
 qplot(stepsByDay, binwidth=1000, xlab='Total Steps per Day')
 ```
 
+![](PA1_template_files/figure-html/unnamed-chunk-2-1.png) 
+
 ## What is the average daily activity pattern?
-```{r}
+
+```r
 stepsByInterval <- sqldf("select interval, avg(steps) as avg_steps from activity_df group by interval")
+```
+
+```
+## Loading required package: tcltk
+```
+
+```r
 qplot(stepsByInterval$interval, stepsByInterval$avg_steps, data = stepsByInterval, geom="line",xlab ="interval", ylab = "steps", main = "Avg Steps Taken by Interval")
+```
+
+![](PA1_template_files/figure-html/unnamed-chunk-3-1.png) 
+
+```r
 stepsByIntervalMax <- sqldf("select interval, avg(steps) as avg_steps from activity_df group by interval order by avg_steps desc limit 1")
 stepsByIntervalMax
 ```
 
+```
+##   interval avg_steps
+## 1      835  206.1698
+```
+
 ## Imputing missing values
-```{r}
+
+```r
 activity_df_na <- sqldf("select count(*) as count_na from activity_df where steps is null")
 
 # missing values
@@ -63,16 +101,32 @@ populateMissingValues <- function(df){
 activity_df_replace_na <- populateMissingValues(activity_df)
 stepsByIntervalReplaceNa <- sqldf("select interval, avg(steps) as avg_steps from activity_df_replace_na group by interval")
 qplot(stepsByIntervalReplaceNa$interval, stepsByIntervalReplaceNa$avg_steps, data = stepsByIntervalReplaceNa, geom="line", xlab ="interval", ylab = "steps", main = "Avg Steps Taken by Interval (replace NA)")
+```
 
+![](PA1_template_files/figure-html/unnamed-chunk-4-1.png) 
+
+```r
 stepsByDayRemoveNa <- tapply(activity_df_replace_na$steps, activity_df_replace_na$date, sum)
 stepsByDayRemoveNaMean <- mean(stepsByDayRemoveNa)
 stepsByDayRemoveNaMedian <- median(stepsByDayRemoveNa)
 stepsByDayMean
+```
+
+```
+## [1] 9354.23
+```
+
+```r
 stepsByDayMedian
 ```
 
+```
+## [1] 10395
+```
+
 ## Are there differences in activity patterns between weekdays and weekends?
-```{r}
+
+```r
 stepsByIntervalDate <- sqldf("select date, interval, avg(steps) as avg_steps from activity_df_replace_na group by date,interval")
 # weekends
 weekendSteps <- subset(stepsByIntervalDate, weekdays(as.Date(stepsByIntervalDate$date)) == "Saturday" | weekdays(as.Date(stepsByIntervalDate$date)) == "Sunday" )
@@ -88,3 +142,5 @@ with(stepsByIntervalWeekend, lines(steps ~ interval, type="l", col="chocolate"))
 with(stepsByIntervalWeekday, lines(steps ~ interval, type="l", col="16" ))  
 legend("topright", lty=c(1,1), col = c("chocolate", "16"), legend = c("weekday", "weekend"), seg.len=3)
 ```
+
+![](PA1_template_files/figure-html/unnamed-chunk-5-1.png) 
--- PeerAssessment1.R
+++ PeerAssessment1.R
@@ -1,49 +1,36 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
-
-
-## Loading and preprocessing the data
-
-```{r}
+# part 1 - loading and processing the data
 setwd("~/JHDataScience/Reproducable_Research/RepData_PeerAssessment1")
+list.files()
 unzip(zipfile="activity.zip")
 activity_df <- read.csv("activity.csv")
 
 library(ggplot2)
 library(sqldf)
-```
 
-## What is mean total number of steps taken per day?
-```{r}
+# part 2 - steps by day
 stepsByDay <- tapply(activity_df$steps, activity_df$date, sum, na.rm=T)
 stepsByDayMean <- mean(stepsByDay)
 stepsByDayMedian <- median(stepsByDay)
 stepsByDayMean
 stepsByDayMedian
+qplot(stepsByDay, xlab='Total Steps per Day')
 
-qplot(stepsByDay, binwidth=1000, xlab='Total Steps per Day')
-```
-
-## What is the average daily activity pattern?
-```{r}
+# part 3 - avg daily activity pattern
 stepsByInterval <- sqldf("select interval, avg(steps) as avg_steps from activity_df group by interval")
+head(stepsByInterval)
 qplot(stepsByInterval$interval, stepsByInterval$avg_steps, data = stepsByInterval, geom="line",xlab ="interval", ylab = "steps", main = "Avg Steps Taken by Interval")
 stepsByIntervalMax <- sqldf("select interval, avg(steps) as avg_steps from activity_df group by interval order by avg_steps desc limit 1")
 stepsByIntervalMax
-```
 
-## Imputing missing values
-```{r}
+# part 4 - inputting missing values
 activity_df_na <- sqldf("select count(*) as count_na from activity_df where steps is null")
+
+activity_df[1,2]
+# notes *1st num = row, 2nd num = column
 
 # missing values
 stepsByIntervalWithMax <- sqldf("select interval, max(steps) as avg_steps from activity_df group by interval")
 
-# function to get max steps by interval to 
 populateMissingValues <- function(df){
     for(i in 1:nrow(df)){
         if(!is.null(df[i,1]) && is.na(df[i,1])){
@@ -61,30 +48,38 @@ populateMissingValues <- function(df){
 }
 
 activity_df_replace_na <- populateMissingValues(activity_df)
+head(activity_df_replace_na)
+head(df)
+df <- activity_df
+nrow(df)
+
 stepsByIntervalReplaceNa <- sqldf("select interval, avg(steps) as avg_steps from activity_df_replace_na group by interval")
-qplot(stepsByIntervalReplaceNa$interval, stepsByIntervalReplaceNa$avg_steps, data = stepsByIntervalReplaceNa, geom="line", xlab ="interval", ylab = "steps", main = "Avg Steps Taken by Interval (replace NA)")
+qplot(stepsByIntervalReplaceNa$interval, stepsByIntervalReplaceNa$avg_steps, data = stepsByIntervalReplaceNa, geom="line",xlab ="interval", ylab = "steps", main = "Avg Steps Taken by Interval")
 
 stepsByDayRemoveNa <- tapply(activity_df_replace_na$steps, activity_df_replace_na$date, sum)
 stepsByDayRemoveNaMean <- mean(stepsByDayRemoveNa)
 stepsByDayRemoveNaMedian <- median(stepsByDayRemoveNa)
 stepsByDayMean
 stepsByDayMedian
-```
 
-## Are there differences in activity patterns between weekdays and weekends?
-```{r}
-stepsByIntervalDate <- sqldf("select date, interval, avg(steps) as avg_steps from activity_df_replace_na group by date,interval")
+# part 5 - weekend vs weekday
 # weekends
+stepsByIntervalDate <- sqldf("select date, interval, avg(steps) as avg_steps from activity_df_replace_na group by date,interval")
 weekendSteps <- subset(stepsByIntervalDate, weekdays(as.Date(stepsByIntervalDate$date)) == "Saturday" | weekdays(as.Date(stepsByIntervalDate$date)) == "Sunday" )
 stepsByIntervalWeekend <- sqldf("select interval, avg(avg_steps) as steps from weekendSteps group by interval")
+plot1 <- qplot(stepsByIntervalWeekend$interval, stepsByIntervalWeekend$steps, data = stepsByIntervalWeekend, geom="line", xlab = "5-minute Interval", ylab ="Number of Steps")
 
 # weekdays
 weekendSteps <- subset(stepsByIntervalDate, weekdays(as.Date(stepsByIntervalDate$date)) != "Saturday" | weekdays(as.Date(stepsByIntervalDate$date)) != "Sunday" )
 stepsByIntervalWeekday <- sqldf("select interval, avg(avg_steps) as steps from weekendSteps group by interval")
+plot2 <- qplot(stepsByIntervalWeekday$interval, stepsByIntervalWeekday$steps, data = stepsByIntervalWeekday, geom="line", xlab = "5-minute Interval", ylab ="Number of Steps")
 
 par(mfrow=c(1,1))  
 with(stepsByIntervalDate, plot(avg_steps ~ interval, type="n", main="Weekday vs. Weekend Avg.", ylab = "steps", ylim=c(0,300)))  
 with(stepsByIntervalWeekend, lines(steps ~ interval, type="l", col="chocolate"))  
 with(stepsByIntervalWeekday, lines(steps ~ interval, type="l", col="16" ))  
 legend("topright", lty=c(1,1), col = c("chocolate", "16"), legend = c("weekday", "weekend"), seg.len=3)
-```
+
+
+    
+    
